ee39e31b96a7863b37de8e41a3c8a84a197386b0
Nosenti <inno100os@gmail.com>
2024-06-19 21:00:44 +0200

    formatting

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,8 +4,20 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API to find the number of
+    subscribers to a given subreddit.
+    Args:
+        subreddit (str): The subreddit to query.
+    Returns:
+        int: The number of subscribers or 0 if the subreddit is invalid.
+    """
     url = "https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url)
+    headers = {
+        "User-Agent":
+        "Mozilla/5.0 (Ubuntu 20.04; Python/3.4.3) MySubredditCounter/0.1"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         return data.get("data", {}).get("subscribers", 0)
