4298402d9bcd45c083f3d3bb1707b697d0470b70
Nosenti <inno100os@gmail.com>
2024-08-13 22:20:35 +0200

    refactor

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-"""
-    Uses Reddit API to print the number of subscribers of a subreddit
-"""
+"""Fetching the number of subscribers of a subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Get the number of subscribers for a given subreddit
+    Queries the Reddit API to find the number of
+    subscribers to a given subreddit.
+
+    Args:
+        subreddit (str): The subreddit to query.
+
+    Returns:
+        int: The number of subscribers or 0 if the subreddit is invalid.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"user-agent": "request"}
+    headers = {
+        "User-Agent":
+        "Mozilla/5.0 (Ubuntu 20.04; Python/3.4.3) MySubredditCounter/0.1"
+    }
+
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("data", {}).get("subscribers", 0)
+    else:
         return 0
-
-    data = response.json().get("data")
-    num_subs = data.get("subscribers")
-
-    return num_subs
